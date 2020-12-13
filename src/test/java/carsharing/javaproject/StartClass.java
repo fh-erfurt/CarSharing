57f90c57ea5a41ed5f18c9e8a4f320e66e3c55d3
Niklas <nowackniklas@gmail.com>
2020-12-13 21:11:56 +0100

    Struktur

---
+++ src/test/java/carsharing/javaproject/StartClass.java
@@ -0,0 +1,13 @@
+package carsharing.javaproject;
+
+public class StartClass {
+
+    void check_if_something_does_something() {
+        // Given
+
+        // When
+
+        // Then
+        //
+    }
+}
