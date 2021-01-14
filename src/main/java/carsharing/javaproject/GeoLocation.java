3da56e9a0c6d5f9837e237a65ad25330b1d7b792
Niklas <nowackniklas@gmail.com>
2021-01-14 22:25:15 +0100

    Classes: Car, CarManager, GeoLocation modified. Added global var in StartClass.

--- src/main/java/carsharing/javaproject/GeoLocation.java
+++ src/main/java/carsharing/javaproject/GeoLocation.java
@@ -4,20 +4,59 @@ import java.time.*;
 
 public class GeoLocation {
 
-    private LocalDate lastSeen;
+    private LocalDateTime lastSeen;
 
     private float latitude, longitude;
 
     public GeoLocation() {
-
+        //By default the last seen time will set to the current timestamp.
+        this.lastSeen = LocalDateTime.now();
     }
 
-    void set () {
+    //Constructor witch can be called and all needed variables be set.
 
+    public GeoLocation(float _lat, float _long, LocalDateTime _date) {
+        this.latitude = _lat;
+        this.longitude = _long;
+
+    //last seen Time can be optionally set in parameter. If its not set, the current timestamp will be used!
+
+        if (_date == null) {
+            this.lastSeen = LocalDateTime.now();
+        }
+        else {
+
+            this.lastSeen= _date;
+        }
     }
 
-    void get () {
+    // Getter / Setter -----------------------------------------------
 
+    public LocalDateTime getLastSeen() {
+        return lastSeen;
     }
+
+    public void setLastSeen(LocalDateTime _lastSeen) {
+        this.lastSeen = _lastSeen;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float _long) {
+        this.longitude = _long;
+    }
+
+    public void setLatitude (float _lat) {
+        this.latitude = _lat;
+    }
+
+    public float getLatitude () {
+
+        return this.latitude;
+    }
+
+
 
 }
--- src/main/java/carsharing/javaproject/StartClass.java
+++ src/main/java/carsharing/javaproject/StartClass.java
@@ -2,9 +2,15 @@ package carsharing.javaproject;
 
 public class StartClass {
 
+    public static CarManager globalCarManager;
+    public static final int MAX_RESERVATION_MINUTES = 30;
+
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+
+
+
     }
 
 }
