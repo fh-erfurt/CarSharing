f5470669e0e4e56e38f60d1e518e05d6cf7aa33d
Niklas <nowackniklas@gmail.com>
2021-02-01 09:04:21 +0100

    Added JavaDoc

---
+++ Docs/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"auth"},{"l":"data"},{"l":"exception"},{"l":"handler"},{"l":"history"},{"l":"model.location"},{"l":"model.trip"},{"l":"model.user"},{"l":"outofbandcode"},{"l":"payment"},{"l":"task"},{"l":"test"}];updateSearchResults();
