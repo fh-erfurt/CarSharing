69d125f06540b2d298c92566cc4a4cc2d8cf203c
Dave Akinyemi <72821087+daveakinyemi@users.noreply.github.com>
2021-06-14 16:13:51 +0200

    Delete Docs directory

--- Docs/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"auth"},{"l":"data"},{"l":"exception"},{"l":"handler"},{"l":"history"},{"l":"model.location"},{"l":"model.trip"},{"l":"model.user"},{"l":"outofbandcode"},{"l":"payment"},{"l":"task"},{"l":"test"}];updateSearchResults();
