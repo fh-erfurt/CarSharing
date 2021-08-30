9afaba7199eced2cfe64d08cd398475bd4724351
= <=>
2021-08-30 09:42:20 +0200

    servlet initializer

--- src/main/java/com/javaii/carsharing/CarsharingApplication.java
+++ src/main/java/com/javaii/carsharing/CarsharingApplication.java
@@ -2,8 +2,15 @@ package com.javaii.carsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.javaii.carsharing.controller",
+		"com.javaii.carsharing.model",
+		"com.javaii.carsharing.repository",
+		"com.javaii.carsharing.service"
+})
 public class CarsharingApplication {
 
 	public static void main(String[] args) {
